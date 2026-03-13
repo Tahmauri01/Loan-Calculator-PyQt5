4cf1bad39a18201a7024d1669eab02c556aae33c
Tahmauri <tmbo718@gmail.com>
2026-03-12 23:14:05 -0400

    Created window for app, made app exitable

--- main.py
+++ main.py
@@ -5,3 +5,28 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
 import sys
+
+#main class
+class Window(QMainWindow):
+    #Constructor method
+    def __init__(self):
+        #Inherits method from superclass (QMainWindow)
+        super().__init__()
+        #Setting title
+        self.setWindowTitle("Loan Calculator")
+        #Setting geometry
+        self.width = 400
+        self.hieght = 500
+        self.setGeometry(100, 100, self.width, self.hieght)
+        #Show window
+        self.show()
+
+
+#Create app object, initializes Qt app
+App = QApplication(sys.argv)
+
+#Create window class
+window = Window()
+
+#Start the app, makes sure everything is blocked until the app is exited
+sys.exit(App.exec())
