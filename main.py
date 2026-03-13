bbb9f642cb1b18aef546645974f30f41a96c1609
Tahmauri <tmbo718@gmail.com>
2026-03-13 01:14:50 -0400

    Added title, created interest label "Annual Interest"

--- main.py
+++ main.py
@@ -18,8 +18,43 @@ class Window(QMainWindow):
         self.width = 400
         self.hieght = 500
         self.setGeometry(100, 100, self.width, self.hieght)
+        self.UiComponents()
         #Show window
         self.show()
+
+    #Function for adding widgets
+    def UiComponents(self):
+        head = QLabel("Loan Calculator \n XYZ Bank", self) #Title on the app
+        head.setGeometry(0, 10, 400, 60) #x, y, width, hieght
+        font = QFont("Times", 15) #font
+        font.setBold(True)
+        head.setFont(font)
+        head.setAlignment(Qt.AlignCenter) #centers head
+        color = QGraphicsColorizeEffect(self) #color
+        color.setColor(Qt.green) 
+        head.setGraphicsEffect(color)
+        #Creates Intrest Label
+        i_label = QLabel("Annual Interest", self)
+        #Properties of Interest Label
+        i_label.setAlignment(Qt.AlignCenter)
+        i_label.setGeometry(20, 100, 170, 40)
+        i_label.setStyleSheet("QLabel" "{"
+                            "border : 2px solid black;"
+                            "background: rbga(70, 70, 70, 35);"
+                                        "}")
+        
+        i_label.setFont(QFont("Times", 9))
+
+
+
+
+
+
+
+
+
+
+
 
 
 #Create app object, initializes Qt app
